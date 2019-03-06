3525a4cfa43898505511aadbf7447bc345f5791f
A-Fredette <andrew.fredett@gmail.com>
2019-03-06 23:18:38 +0100

    Fixed typos; confirmed URL query

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
-<!-- Everything gets directed through this file -->
 <?php
-    require_once '../app/bootstrap.php'; /* But really it comes from bootstrap.php */
+  require_once '../app/bootstrap.php';
 
-// Init Core Library
-
-    $init = new Core;
+  // Init Core Library
+  $init = new Core;

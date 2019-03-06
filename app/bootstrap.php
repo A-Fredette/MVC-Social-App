d9dc75a77eb843c0316bb1dc169a51164eed8a89
A-Fredette <andrew.fredett@gmail.com>
2019-03-06 20:11:59 +0100

    Renamed library folder; cleaned up code

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,2 +1,5 @@
-<!-- Requires all necessary files -->
-HELLO
+<?php
+    //Load Libraries
+    require_once 'libraries/core.php';
+    require_once 'libraries/controller.php';
+    require_once 'libraries/database.php';
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <!-- Everything gets directed through this file -->
 <?php
     require_once '../app/bootstrap.php'; /* But really it comes from bootstrap.php */
+
+// Init Core Library
+
+    $init = new Core;
